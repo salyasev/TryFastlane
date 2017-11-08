6f4b62828c658d566fb36e15e52a06a4122d8c15
Sergey Alyasev <sergey.alyasev@convergent-usa.com>
2017-11-08 10:15:03 +0300

    Initial Commit

---
+++ TryFastlane/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TryFastlane
+//
+//  Created by Sergey Alyasev on 08/11/2017.
+//  Copyright © 2017 Convergent Media Group. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
